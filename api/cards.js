00d42cc20202cf793261ba729db7b8723aa77978
fazenn <66947505+lolololo967@users.noreply.github.com>
2026-04-06 14:01:49 +0200

    Update cards.js

--- api/cards.js
+++ api/cards.js
@@ -55,6 +55,8 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
+
   if (req.method === 'OPTIONS') return res.status(200).end();
 
   try {
